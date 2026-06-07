141d8cf5ed8ed735b7decd58fda74f029bdc0a60
ArielLerner <139831545+ArielLerner@users.noreply.github.com>
2026-06-07 17:43:35 -0300

    Update Tree_Distances_1.cpp

--- Tree_Distances_1.cpp
+++ Tree_Distances_1.cpp
@@ -1,71 +1,57 @@
-// solucion de diciembre de 2025
 #include <bits/stdc++.h>
-#define forr(i,a,b) for(int i = a; i < b; i++)
+#define forr(i,a,b) for(int i = (a); i < (b); i++)
 #define forn(i,n) forr(i,0,n)
-#define dforr(i,a,b) for(int i = b-1; i >=a;i--)
+#define dforr(i,a,b) for(int i = (b-1); i >= (a); i--)
 #define dforn(i,n) dforr(i,0,n)
+#define all(x) x.begin(),x.end()
+#define pb push_back
+#define sz(i) i.size()
 using namespace std;
-vector<vector<int>> c;
-vector<int> maxDist;
-vector<int> maxToHijos;
-vector<int> segMax;
-int maximoToHijo(int n, int padre){
-    int maximo = 0; int iMax = -1;
-    int minimo = 0;
-    for(int hijo : c[n]){
-        if(hijo == padre){continue;}
-        int hastaHijo = maximoToHijo(hijo,n) +1;
-        if(hastaHijo > maximo){minimo = maximo; maximo = hastaHijo; iMax = hijo;}
-        else{
-            if(hastaHijo > minimo){minimo = hastaHijo;}
-        }
-    }
-    maxToHijos[n] = maximo;
-    segMax[n] = minimo;
-    return maximo;
-}
-void maxTotal(int n, int padre, int maxDistPadre){
-    bool aPadre = false;
-    if(n == 0){
-        maxDist[n] = maxToHijos[n];
-    }
-    else{
-        if(maxDistPadre > maxToHijos[n]){
-            aPadre = true;
-            maxDist[n] = maxDistPadre;
-        }
-        else{
-            maxDist[n] = maxToHijos[n];
-        }
-    }
-    for(int hijo : c[n]){
-        if(hijo == padre){continue;}
-        if(!aPadre && maxDist[n] == maxToHijos[hijo] + 1){ // si viene del hijo
-            maxTotal(hijo,n,max(maxDistPadre + 1, segMax[n] + 1));
-        }
-        else{
-            maxTotal(hijo,n,maxDist[n] + 1);
-        }
+using par = pair<int,int>;
+using ll = long long int;
+vector<vector<int>> ady;
+vector<int> dist;
+void searchD(int i, int p , int d){
+    dist[i] = d;
+    for(int c : ady[i]){
+        if(c == p){continue;}
+        searchD(c,i,d+1);
     }
 }
 int main(){
    //freopen("C:/Users/Ari/Desktop/Proyectos/C++/input/stdin.txt","r",stdin);
     int n; cin >> n;
-    c.resize(n);
-    maxDist.resize(n,0);
-    maxToHijos.resize(n,0);
-    segMax.resize(n,0);
-    forr(i,1,n){
+    ady.resize(n);
+    dist.resize(n);
+    forn(i,n-1){
         int a,b;
-        cin >> a; a--;
-        cin >> b; b--;    
-        c[a].push_back(b);
-        c[b].push_back(a);
+        cin >> a >> b;
+        a--;b--;
+        ady[a].pb(b);
+        ady[b].pb(a);
     }
-    maximoToHijo(0,-1);
-    maxTotal(0,-1,0);
-    for(int d : maxDist){
-        cout << d << " ";
+    searchD(0,-1,0); 
+    // busco la distancia del 0 con todos
+    // porque el que mayor distancia tiene es parte de un diametro posible
+    int a = 0;
+    forn(i,sz(dist)){
+        if(dist[i] > dist[a]){
+            a = i;
+        }
+    }
+    searchD(a,-1,0); // busco ahora su complemento del diametro que es el nodo con mayor distancia de a
+    vector<int> distA = dist;
+    int b = a;
+    forn(i,sz(dist)){
+        if(dist[i] > dist[b]){
+            b = i;
+        }
+    }
+    searchD(b,-1,0);
+    vector<int> distB = dist;
+    forn(i,n){
+        int dMax = max(distA[i],distB[i]) ;
+        cout << dMax << " ";
     }
     cout << endl;
 }
