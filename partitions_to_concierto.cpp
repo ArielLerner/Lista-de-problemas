4e3f1e92368d37f6312a7bdb59238677924c51d9
ArielLerner <139831545+ArielLerner@users.noreply.github.com>
2026-06-07 11:54:31 -0300

    Update partitions_to_concierto.cpp

--- partitions_to_concierto.cpp
+++ partitions_to_concierto.cpp
@@ -11,14 +11,30 @@ using par = pair<int,int>;
 using ll = long long int;
 char redonda = 'r';
 char blanca = 'b';
+string blancaP = "b*";
+string negra = "n";
 string toConciertos(vector<int> &e){
     string ans = "";
-    for(int x : e){
-        ans += blanca; // para que todas las redondas deban ir al mismo lado
+    forn(i,sz(e)){
+        int x = e[i];
+        if(x == 0){continue;}
+        if(i % 2 == 0){// para que todas las redondas deban ir al mismo lado
+            ans += blanca; 
+        }
+        else{
+            ans+= blancaP;
+        }
         forn(i,x+1){ // agrego 1 de mas para compensar el compas que despues agrego del otro lado
             ans += redonda;
         }
-        ans += blanca; // dejo los dos compases finalizados
+        //  dejo los dos compases finalizados
+        // si trato de dejar los dos en el medio, despues no puedo continuar
+        if(i % 2 == 0){
+            ans += blanca;
+        }
+        else{
+            ans+= negra;
+        }
     }
     return ans;
 }
